97e89cc1ae0066e4dfa172f9102dbce45dfd8195
230500226 <125464717+230500226@users.noreply.github.com>
2023-06-17 21:17:53 +0000

    Q3

---
+++ Q3.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+#include<string.h>
+
+int main() {
+    char str[50];
+    char strReversed[50];
+
+    printf("Please enter a string:\n");
+        scanf("%s", str);
+
+        int j = 0; //to add the end of string line to end of strReversed
+        for (int i=strlen(str)-1; i>=0 ; i--){
+            strReversed[j] = str[i];
+            j++;
+        }
+        strReversed[j] = '\0';
+
+    printf("\nThe string %s in reverse is %s with loop", str, strReversed);
+
+        printf("\nOr just %s is ", str);
+        //printf("%s without loop using strrev\n",strrev(str));
+        //only works on some platforms like windows i think
+    return 0;
+}
